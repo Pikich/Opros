9c24a334868a11292bdd0d6f08bb9197299f0937
Pikich <Pikich00@yandex.ru>
2022-06-26 17:19:51 +0300

    Opros

---
+++ index.js
@@ -0,0 +1,15 @@
+const buttonYes = document.querySelector('.button_yes')
+const buttonNo = document.querySelector('.button_no')
+const title = document.querySelector('.title')
+
+buttonYes.addEventListener('click', () => {
+    title.textContent = 'Всем пиздец!'
+})
+
+buttonNo.addEventListener('click', () => {
+    buttonNo.style.display = 'none'
+    title.textContent = 'Ответ неверный!'
+    setTimeout(() => {
+        title.textContent = 'Ты лютый?'
+    }, 2000)
+})
